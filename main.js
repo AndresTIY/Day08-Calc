f6a980f2b6529accdf5ea4c48e94b58a45a10e42
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-08 16:56:45 -0600

    adding styles

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
-// var input1 = document.forms["0"]["0"].value;
-// var input2 = document.forms["0"][1].value;
-// only gives preexisting value, not the new value unless it gets reassigned
-
-
-// function add (input1, input2) {
-//   var input1 = Number(document.forms["0"]["0"].value);
-//   var input2 = Number(document.forms["0"][1].value);
-//   console.log(input1 + input2);
-// }
-
-
 var result = document.querySelector('#result');
+var x = document.querySelector('#one');
+var y = document.querySelector('#two');
 
-function add (x, y) {
-  var x = Number(document.querySelector('#one').value);
-  var y = Number(document.querySelector('#two').value);
-  result.textContent = (x + y);
+
+function add () {
+  result.textContent = Number(x.value) + Number(y.value);
 }
-
-
-
-// result.textContent = add();
-
-// document.querySelector('cssSelector')
-
-// - let's say <div class="fancy">
-// - if you want to access that element in js
-// `document.querySelector('.fancy')`
-// now we can store it in a var
-// `var fancy = document.querySelector('.fancy')`
